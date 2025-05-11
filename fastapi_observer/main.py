8f46569df2c774f0afa7e25e8c7ef67cb413dfec
daniil <skeal.smirnov2004@mail.ru>
2025-05-11 22:18:28 +0300

    add healthcheck

--- fastapi_observer/main.py
+++ fastapi_observer/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi_observer.logging.middleware import LoggingMiddleware
 from fastapi_observer.logging.setup import setup_logger
 from fastapi_observer.metrics.middleware import MetricsMiddleware, metrics
+from fastapi_observer.schemas import HealthCheck
 from fastapi_observer.tracing.middleware import TracingMiddleware
 from fastapi_observer.tracing.setup import setup_otel
 
@@ -19,3 +20,11 @@ def setup_observer(app: FastAPI, service_name: str) -> None:
     setup_otel(app, service_name)
 
     app.add_route("/metrics", metrics)
+
+    @app.get(
+        "/health",
+        tags=["healthcheck"],
+        response_model=HealthCheck,
+    )
+    def get_health() -> HealthCheck:
+        return HealthCheck()
